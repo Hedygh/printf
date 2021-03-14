b50ddff5c7acf5205becc5fe6b63f6b3a84129e3
hedygh <dyouahes@gmail.com>
2021-03-14 16:56:17 +0100

    handle p option

--- arg_nbr_functions_1.c
+++ arg_nbr_functions_1.c
@@ -1,6 +1,7 @@
 #include <stdarg.h>
 #include "holberton.h"
 #include <limits.h>
+#include <stdlib.h>
 
 /**
  * print_unsigned - manage unsigned int
@@ -13,4 +14,21 @@ int print_unsigned(va_list ap)
 	unsigned int argument = va_arg(ap, unsigned int);
 
 	return (print_unsigned_number(argument));
+}
+/**
+ * print_ptr - print adress
+ * @ap: arg void
+ * Return: number of printed char
+ */
+int print_ptr(va_list ap)
+{
+	char *str;
+	int sum = 0;
+	void *p = va_arg(ap, void*);
+
+	str = convert_base_pointer(p, 16);
+	sum += _puts("0x", 0);
+	sum += _puts(str, 0);
+	free(str);
+	return (sum);
 }
--- main.c
+++ main.c
@@ -7,7 +7,9 @@ int main(void)
 	char c = 'o';
 	char str[] = "toto";
 	int i = 32907978, j = 0, k = -97984372, l = INT_MIN;
-	unsigned int a = -1, b = -233, e = 5, d = 98;
+	unsigned int a = -225, b = 12 , e = 5, d = 98;
+	void *p = &i;
+	void *pp = &str;
 
 	_printf("[string] %s\n hello %c\n", str, c);
 	printf("[string] %s\n hello %c\n", str, c);
@@ -15,15 +17,15 @@ int main(void)
 	printf("[itoa] %s\n", _itoa(j));
 	printf("[itoa] %s\n", _itoa(k));
 	printf("[itoa] %s\n", _itoa(l));
-	_printf("[integer] %i\n%i\n%i\n%i\n", i, j, k, l);
-	printf("[integer] %i\n%i\n%i\n%i\n", i, j, k, l);
-	_printf("[decimal] %d\n%d\n%d\n%d\n", i, j, k, l);
-	printf("[decimal] %d\n%d\n%d\n%d\n", i, j, k, l);
+	_printf("[integer] %i %i %i %i\n", i, j, k, l);
+	printf("[integer] %i %i %i %i\n", i, j, k, l);
+	_printf("[decimal] %d %d %d %d\n", i, j, k, l);
+	printf("[decimal] %d %d %d %d\n", i, j, k, l);
 	_printf("[%%] %%\n");
 	printf("[%%] %%\n");
 	_printf("Unsigned hexadecimal:[%x, %X]\n", a, b);
 	printf("Unsigned hexadecimal:[%x, %X]\n", a, b);
-	_printf("int octal: [%o, %o]\n", i , k); 
+	_printf("int octal: [%o, %o]\n", i , k);
 	printf("int octal: [%o, %o]\n", i , k);
 	_printf("Unsigned binary: [%b, %b]\n", e, d); 
 	printf("%u\n", a);
@@ -32,9 +34,11 @@ int main(void)
 	_printf("carriage return %S\n", "test1 \t test2");
 	_printf("carriage return %S\n", "test1 \b test2");
 	_printf("carriage return %S\n", "test1 \a test2");
-	_printf("carriage return %S\n", "test1 \v test2");
+	_printf("carriage return %S\n", "ned long nb,test1 \v test2");
 	_printf("carriage return %S\n", "test1 \r test2");
 	_printf("%r\n", "Holberton");
+	printf("[adress = %p %p]\n", p, pp);
+	_printf("[adress = %p %p]\n", p, pp);
 
 	return (0);
 }
