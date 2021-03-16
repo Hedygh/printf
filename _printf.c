97657c6ea97d9866ce70cbb5fe38c2936106992c
hedygh <dyouahes@gmail.com>
2021-03-16 18:37:06 +0100

    remove check for single %

--- _printf.c
+++ _printf.c
@@ -51,8 +51,6 @@ int _printf(const char *format, ...)
 	int sum = 0, i = 0;
 	int (*func)(va_list);
 
-	if (format[0] == '%' && format[1] == '\0')
-		return (-1);
 	if (!format)
 		return (-1);
 	va_start(ap, format);
